3765690f557dc0b8399f8f9cea8f061d28aa096a
Walter Fabian Rodriguez Salazar <wfabianrs@gmail.com>
2026-03-02 11:17:21 -0500

    chore: Corregir ajustes visuales

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
+	"unicode/utf8"
 
 	"github.com/integrii/flaggy"
 	"gitlab.com/wfrsgo/templet/internal/project"
 	"gitlab.com/wfrsgo/templet/internal/tui"
-	"gitlab.com/wfrsgo/templet/internal/utils"
 )
 
 var Version = "devel"
@@ -17,10 +19,15 @@ var Version = "devel"
 func main() {
 	var name string
 	var source string
+	var repo string
 
 	slog.SetLogLoggerLevel(slog.LevelDebug)
 
+	flaggy.SetName(nombreEjecutable())
+	flaggy.SetDescription("Herramienta para la creación de estructuras de archivos a partir de plantillas")
+
 	flaggy.String(&name, "n", "name", "Nombre del proyecto (obligatorio)")
+	flaggy.String(&repo, "r", "repo", "Ruta del repositorio git donde descargar la plantilla (host[:puerto], opcional)")
 
 	flaggy.AddPositionalValue(&source, "source", 1, true, "nombre de la plantilla a usar en formato `grupo/nombre`")
 	flaggy.ShowHelpOnUnexpectedDisable()
@@ -36,10 +43,7 @@ func main() {
 		exit("La plantilla es obligatoria")
 	}
 
-	cfg, err := utils.ReadConfig()
-	exitErr(err)
-
-	prj, err := project.New(source, cfg.Routes)
+	prj, err := project.New(source, repo)
 	exitErr(err)
 
 	defer prj.Delete()
@@ -49,6 +53,10 @@ func main() {
 
 	meta, err := project.ReadMeta(prj.Dir())
 	exitErr(err)
+
+	title := fmt.Sprintf("Asistente para la creación del proyecto %s:", name)
+	fmt.Println(tui.Colorize("<red><bold>%s\n%s</>", title, strings.Repeat("=", utf8.RuneCountInString(title))))
+	fmt.Println()
 
 	data, err := meta.Variables.Form()
 	exitErr(err)
@@ -60,7 +68,11 @@ func main() {
 
 	fmt.Println(
 		tui.Colorize(
-			fmt.Sprintf("<green>✔ Proyecto creado con éxito a las %s</>\n", time.Now().Format("01/02/2006 03:04:05 PM")),
+			fmt.Sprintf(
+				"<green>✔ Proyecto <bold>%s</><green> creado con éxito a las %s</>\n",
+				name,
+				time.Now().Format("01/02/2006 03:04:05 PM"),
+			),
 		),
 	)
 }
@@ -75,4 +87,17 @@ func exitErr(err error) {
 		slog.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+// nombreEjecutable devuelve el nombre del archivo ejecutable del programa sin extensión.
+func nombreEjecutable() string {
+	var name string
+	exePath, err := os.Executable()
+	if err != nil {
+		// En caso de error, recurrimos a os.Args[0]
+		name = filepath.Base(os.Args[0])
+	} else {
+		name = filepath.Base(exePath)
+	}
+	return strings.TrimSuffix(name, filepath.Ext(name))
 }
